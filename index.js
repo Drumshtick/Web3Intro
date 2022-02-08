8c5a5c0f404c5fd8e8e691fe733bb7ee8f3ae47b
Drumshtick <ekodnar@outlook.com>
2022-02-08 16:04:03 -0600

    created connection to burning wallet 'dead' and checked the balance after converting wei to Ether

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 require('dotenv').config();
 const Web3 = require('web3');
 
+// Initiate new connection
+const newWeb3Connection = new Web3(process.env.rpcURL)
 
-console.log(process.env.TEST);
+// Address that Eth users use for burning tokens
+const burningAddress = '0x000000000000000000000000000000000000dead';
+
+// Check account balance
+newWeb3Connection.eth.getBalance(burningAddress, (err, wei) => {
+  balance = newWeb3Connection.utils.fromWei(wei, 'ether');
+  console.log(`Address: ${burningAddress} has a balance of ${balance} Ether`);
+});
