e7007acf64aedfa49fdb1fab136f063fe2f8bf0a
Drumshtick <ekodnar@outlook.com>
2022-02-08 16:14:09 -0600

    accepts cmd line args

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 require('dotenv').config();
 const Web3 = require('web3');
 
+let args, burningAddress;
+if (process.argv.length > 2) {
+  args = process.argv[2];
+}
+
+if (!args) {
+  // Address that Eth users use for burning tokens
+  burningAddress = '0x000000000000000000000000000000000000dead';
+}
+
 // Initiate new connection
 const newWeb3Connection = new Web3(process.env.rpcURL)
 
-// Address that Eth users use for burning tokens
-const burningAddress = '0x000000000000000000000000000000000000dead';
+
 
 // Check account balance
-newWeb3Connection.eth.getBalance(burningAddress, (err, wei) => {
+newWeb3Connection.eth.getBalance(args ? args : burningAddress, (err, wei) => {
   // Second arg is the smallest denomination of ether
   if (err) {
     console.log("ERROR in getBalance: ", err)
   }
   // Convert wei to Ether with fromWei
   balance = newWeb3Connection.utils.fromWei(wei, 'ether');
-  console.log(`Address: ${burningAddress} has a balance of ${balance} Ether`);
+  console.log(`Address: ${args || burningAddress} has a balance of ${balance} Ether`);
 });
