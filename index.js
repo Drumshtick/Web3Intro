7a1d7683840092d1922864a101d42691bf3cfd6a
Drumshtick <ekodnar@outlook.com>
2022-02-08 16:18:21 -0600

    implemented check for wallet address validity

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 require('dotenv').config();
 const Web3 = require('web3');
 
+// Initiate new connection
+const newWeb3Connection = new Web3(process.env.rpcURL)
+
 let args, burningAddress;
+
 if (process.argv.length > 2) {
   args = process.argv[2];
+  const isValid = newWeb3Connection.utils.isAddress(args);
+  if (!isValid) {
+    console.log("Invalid address: ", args);
+    console.log("Ensure wallet address is valid")
+    return;
+  } 
 }
 
 if (!args) {
@@ -11,15 +21,12 @@ if (!args) {
   burningAddress = '0x000000000000000000000000000000000000dead';
 }
 
-// Initiate new connection
-const newWeb3Connection = new Web3(process.env.rpcURL)
-
-
 
 // Check account balance
 newWeb3Connection.eth.getBalance(args ? args : burningAddress, (err, wei) => {
   // Second arg is the smallest denomination of ether
   if (err) {
+    console.log("HERE")
     console.log("ERROR in getBalance: ", err)
   }
   // Convert wei to Ether with fromWei
