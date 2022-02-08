c231ed8bd974ac3ea57dfb32633d88f8ce0ad304
Drumshtick <ekodnar@outlook.com>
2022-02-08 16:05:45 -0600

    added notes for future ref

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ const burningAddress = '0x000000000000000000000000000000000000dead';
 
 // Check account balance
 newWeb3Connection.eth.getBalance(burningAddress, (err, wei) => {
+  // Second arg is the smallest denomination of ether
+  if (err) {
+    console.log("ERROR in getBalance: ", err)
+  }
+  // Convert wei to Ether with fromWei
   balance = newWeb3Connection.utils.fromWei(wei, 'ether');
   console.log(`Address: ${burningAddress} has a balance of ${balance} Ether`);
 });
